7aa0003ce5d3c5d0eff090024d10e00b4211e300
iaintshine <bodziomista@gmail.com>
2015-06-01 13:51:05 +0200

    SyncService and Sync implementations - TBT

--- basecrm/sync.py
+++ basecrm/sync.py
@@ -27,7 +27,12 @@ class SyncService(object):
         :return: Dictionary that support attribute-style access and represents newely created Synchronization Session or None.
         :rtype: dict
         """
-        return None
+
+        status_code, _, session = self.http_client.post('/sync/start',
+                                                        body=None,
+                                                        headers=self.build_headers(device_uuid))
+
+        return None if status_code == 204 else session
 
     def fetch(self, device_uuid, session_id):
         """
@@ -45,7 +50,13 @@ class SyncService(object):
             Empty list if there is no more data to synchronize.
         :rtype: list
         """
-        return []
+
+        status_code, _, root = self.http_client.get("/sync/{session_id}/queues/main".format(session_id=session_id),
+                                                    params=None,
+                                                    headers=self.build_headers(device_uuid),
+                                                    raw=True)
+
+        return [] if status_code == 204 else root['items']
 
     def ack(self, device_uuid, ack_keys):
         """
@@ -60,13 +71,32 @@ class SyncService(object):
         :return: True if the operation succeeded.
         :rtype: bool
         """
-        return False
+
+        attributes = {'ack_keys': ack_keys}
+        status_code, _, _ = self.http_client.post('/sync/ack',
+                                                  body=attributes,
+                                                  headers=self.build_headers(device_uuid))
+
+        return status_code == 202
+
+    def build_headers(self, device_uuid):
+        return {'X-Basecrm-Device-UUID': device_uuid}
 
 
 class Sync(object):
     """
     :class:`basecrm.Sync` is a BaseCRM Sync API v2 high-level wrapper.
     """
+
+    """
+    Constant representing acknowledged state.
+    """
+    ACK = True
+
+    """
+    Constant representing not acknowledged state.
+    """
+    NACK = False
 
     def __initialize__(self, client, device_uuid):
         """
@@ -91,13 +121,39 @@ class Sync(object):
         .. code-block:: python
             :linenos:
 
-            client = basecrm.Client(access_token='<YOUR_PERSONAL_ACCESS_TOKEN>')
-            sync = basecrm.Sync(client=client, device_uuid='<YOUR_DEVICES_UUID>')
-
-            sync.fetch(lambda meta, data: return ack)
+            >>> client = basecrm.Client(access_token='<YOUR_PERSONAL_ACCESS_TOKEN>')
+            >>> sync = basecrm.Sync(client=client, device_uuid='<YOUR_DEVICES_UUID>')
+            >>> sync.fetch(lambda meta, data: return basecrm.Sync.ACK)
 
         :param callback: Callback that will be called for every item in a queue.
             Takes two input arguments: synchronization meta data and assodicated data.
             It must return either ack or nack.
         """
-        return
+
+        # Set up a new synchronization session for a given device's UUID
+        session = self.client.sync.start(self.device_uuid)
+
+        # Check if there is anything to synchronize
+        if session is None or 'id' not in session:
+            return
+
+        # Drain the main queue until there is no more data (empty array)
+        while True:
+            # Fetch the main queue
+            queue_items = self.client.sync.fetch(self.device_uuid, session['id'])
+
+            # nothing more to synchronize ?
+            if not queue_items:
+                break
+
+            # let client know about both data and meta
+            ack_keys = []
+
+            for item in queue_items:
+                if callback(item['meta'], item['data']):
+                    ack_keys.append(item['meta']['sync']['ack_key'])
+
+            # As we fetch new data, we need to send acknowledgement keys
+            # if any ..
+            if ack_keys:
+                self.client.sync.ack(self.device_uuid, ack_keys)
