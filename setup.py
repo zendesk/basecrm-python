07ec5e59d4936f267c9f7acc1b318d8d28bb6fd3
Lukáš Doležal <ldolezal@zendesk.com>
2020-05-13 11:06:37 +0200

    v1.2.9 (#36)
    fix license classfier to Apache License in package metadata

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='basecrm',
-    version='1.2.8',
+    version='1.2.9',
     description='BaseCRM Official API V2 library client for Python',
     long_description=README,
     long_description_content_type="text/markdown",
@@ -25,7 +25,7 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
